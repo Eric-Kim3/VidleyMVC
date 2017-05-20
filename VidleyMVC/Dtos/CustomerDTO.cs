0b3b9a3b41c90128cae5b2edbae064dfc0ffe34e
Eric Kim <eric.kim3@outlook.com>
2017-05-20 15:51:26 -0400

    Fifth Commit Using DTOs and IHTTP

---
+++ VidleyMVC/Dtos/CustomerDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using VidleyMVC.Models;
+
+namespace VidleyMVC.Dtos
+{
+    public class CustomerDTO
+    {
+        public int Id { get; set; }
+
+        //[Min18YearsIfAMember]
+        public DateTime? Birthdate { get; set; }
+
+        [Required(ErrorMessage = "Please enter customer's name.!.!.")]//making it not nullable
+        [StringLength(255)] //String Length cannot be more than 255 
+        public string Name { get; set; }
+        public bool IsSubscribedToNewsLetter { get; set; }
+        public byte MembershipTypeId { get; set; }
+    }
+}
---
+++ VidleyMVC/Dtos/MovieDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using VidleyMVC.Models;
+
+namespace VidleyMVC.Dtos
+{
+    public class MovieDTO
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+        [Required]
+        public DateTime DateAdded { get; set; }
+        [Required]
+        [Range(1, 20)]
+        public int NumberInStock { get; set; }
+        [Required]
+        public byte GenreTypeId { get; set; }
+    }
+}
